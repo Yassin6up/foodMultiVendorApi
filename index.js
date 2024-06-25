9373abb151808aedc22250f9f4756d7865840856
Yassine <110790693+Yassin6up@users.noreply.github.com>
2024-06-25 04:38:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = 3000;
 
 
 const db = mysql.createConnection({
-  host: '5.9.215.4',
+  host: 'fast-path.info',
   user: 'fastvgu_yassine',
   password: 'yassine6up',
   database: 'fastvgu_fast_path'
